7dcb0b32e210ad22f8e76e638ab22ff6a94ac4ce
rhatagunde <80765104+rhatagunde@users.noreply.github.com>
2026-04-20 12:03:41 +0530

    add filr for code deploy

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker push rhatagunde/simple-python-flask-app:tagname
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 rhatagunde/simple-python-flask-app
